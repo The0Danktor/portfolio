7848c9724722949b3f28049a1929ebd3690a74d2
The0Danktor <94552546+The0Danktor@users.noreply.github.com>
2023-03-07 18:36:17 +0100

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,23 +1,33 @@
 import NavBar from "../Components/NavBar";
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 
 export function HomePage() {
   return (
-    <div className="flex-row h-full w-auto bg-stone-100 text-black ">
+    <div className=" font-mono flex flex-col h-full w-auto bg-stone-100 text-black ">
       <NavBar />
-      <div className="flex-col  justify-center w-auto">
-        <div className="flex-row w-full justify-center">
-          <div className="flex-row items-center justify-center w-full">
-            <div className="">
-              <h5>I am</h5>
-              <h2>Jamie de Leest</h2>
-              <p>Computer Software Engineer</p>
-              <a href="">MY PROJECTS</a>
+      <div className="flex flex-col  justify-center w-auto bg-stone-100">
+        <div className="flex flex-row w-full justify-center">
+          <div className="flex flex-row justify-around items-center mx-20 my-10 w-full ">
+            <div className="flex flex-col">
+              <h5 className="text-lg">I am</h5>
+              <h2 className="text-4xl ">Jamie de Leest</h2>
+              <p className="text-base my-3  text-gray-500">
+                Computer Software Engineer
+              </p>
+              <div className="flex">
+                <NavLink
+                  className="text-white bg-gradient-to-l from-cyan-500 to-blue-500 p-3 hover:text-white hover:shadow-lg hover:shadow-blue-300 transition duration-300"
+                  to="/Projects"
+                >
+                  MY PROJECTS
+                </NavLink>
+              </div>
             </div>
-            <div className=" bg-slate-600 w-96 h-96"></div>
+            <div className="bg-slate-600 w-96 h-96"></div>
           </div>
         </div>
-        <div>
+        <div className="bg-white">
           <h1>PERSONAL DETAILS</h1>
           <div>
             <p>
